34ad17113ac44da1d17f65037ad00cc5057dca24
Antoniel Aureliano <tonielaureliano@gmailcom>
2025-07-11 23:04:24 -0300

    feat(filmCard): render local poster images using film title mapping.

--- src/components/FilmsCard/index.jsx
+++ src/components/FilmsCard/index.jsx
@@ -1,22 +1,25 @@
 import styles from "./FilmsCard.module.css";
+import { filmsImages } from "../../data/FilmsImages.js"
 
 export const FilmsCard = ({ film }) => {
     return (
         <div className={styles.card}>
             <div>
                 <div>
-                    <img src="" alt="" />
+                    <img className={styles.cardImg} src={filmsImages[film.title]} alt={film.title} />
+                    {console.log('caminho: ', filmsImages[film.title])}
+                    {console.log('título: ', film.title)}
                 </div>
                 <div>
-                    <h2>{film.title}</h2>
-                    <h3>Diretor: {film.director}</h3>
+                    <h2 className={styles.cardTitle}>{film.title}</h2>
+                    <h3><strong>Diretor:</strong> {film.director}</h3>
                     <div>
-                        <p>Ano: {film.release_date}</p>
-                        <p>Duração: {film.running_time} min</p>
+                        <p><strong>Ano:</strong> {film.release_date}</p>
+                        <p><strong>Duração:</strong> {film.running_time} min</p>
                     </div>
                 </div>
             </div>
-            <p>{film.description}</p>
+            
 
         </div>
     )
